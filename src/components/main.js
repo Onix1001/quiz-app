b8077d13d6236df8160f6783b8f01f574c84d199
Onix1001 <caunurs@gmail.com>
2022-12-10 08:17:20 +0700

    up 10 dec

--- src/components/main.js
+++ src/components/main.js
@@ -23,7 +23,7 @@ export default function Main() {
                                 <span className="hamburger-line transition duration-300 ease-in-out"></span>
                                 <span className="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
                             </button>
-                            <nav id="nav-menu" className={!menu ? 'hidden' : "absolute bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full"}>
+                            <nav id="nav-menu" className={menu ? 'hidden' : "absolute bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full"}>
                                 <ul className="block rounded-lg">
                                     <li className="group">
                                         <a href="#Home" className="text-base text-dark py-2 px-8 flex group-hover:bg-light">Profile</a>
